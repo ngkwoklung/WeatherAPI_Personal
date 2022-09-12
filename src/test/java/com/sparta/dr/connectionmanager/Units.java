94c2e6cc1a7dedc707f26857fc20925a7a07e0cb
Jimmy Ng <ngkwoklung@gmail.com>
2022-09-12 16:53:42 +0100

    working on enums

--- src/test/java/com/sparta/dr/connectionmanager/Units.java
+++ src/test/java/com/sparta/dr/connectionmanager/Units.java
@@ -3,5 +3,15 @@ package com.sparta.dr.connectionmanager;
 public enum Units {
     STANDARD,
     METRIC,
-    IMPERIAL,
+    IMPERIAL;
+
+    private String name;
+
+    Units(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
