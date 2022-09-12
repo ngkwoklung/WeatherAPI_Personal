0d7cb958cb40b95e99ec77a8fff3365710465b6d
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-09-12 15:10:58 +0100

    Added JavaDoc to Main DTO

--- src/test/java/com/sparta/dr/DTO/Main.java
+++ src/test/java/com/sparta/dr/DTO/Main.java
@@ -28,38 +28,74 @@ public class Main{
 	@JsonProperty("temp_max")
 	private Double tempMax;
 
+	/**
+	 * Get temperature
+	 * @return temp
+	 */
 	public Double getTemp(){
 		return temp;
 	}
 
+	/**
+	 * Get minimum temperature
+	 * @return temp_min
+	 */
 	public Double getTempMin(){
 		return tempMin;
 	}
 
+	/**
+	 * Get ground level
+	 * @return grnd_level
+	 */
 	public Integer getGrndLevel(){
 		return grndLevel;
 	}
 
+	/**
+	 * Get humidity level
+	 * @return humidity
+	 */
 	public Integer getHumidity(){
 		return humidity;
 	}
 
+	/**
+	 * Get pressure level
+	 * @return pressure
+	 */
 	public Integer getPressure(){
 		return pressure;
 	}
 
+	/**
+	 * Get sea level
+	 * @return sea_level
+	 */
 	public Integer getSeaLevel(){
 		return seaLevel;
 	}
 
+	/**
+	 * Obtain the information about how temperature feels like
+	 * @return feels_like
+	 */
 	public Double getFeelsLike(){
 		return feelsLike;
 	}
 
+	/**
+	 * Get maximum temperature
+	 * @return temp_max
+	 */
 	public Double getTempMax(){
 		return tempMax;
 	}
 
+	/**
+	 * Obtain all the information as one string.
+	 * @return all values as a string
+	 */
 	@Override
  	public String toString(){
 		return 
