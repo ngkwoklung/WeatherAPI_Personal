86022c90f24f534b88e2d5504fee31fd11487dcd
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-09-12 15:43:50 +0100

    Added JavaDoc to Rain DTO

--- src/test/java/com/sparta/dr/DTO/Sys.java
+++ src/test/java/com/sparta/dr/DTO/Sys.java
@@ -19,26 +19,50 @@ public class Sys{
 	@JsonProperty("type")
 	private Integer type;
 
+	/**
+	 * Get country code
+	 * @return country
+	 */
 	public String getCountry(){
 		return country;
 	}
 
+	/**
+	 * Get sunrise time in epoch
+	 * @return sunrise
+	 */
 	public Integer getSunrise(){
 		return sunrise;
 	}
 
+	/**
+	 * Get sunset time in epoch
+	 * @return sunset
+	 */
 	public Integer getSunset(){
 		return sunset;
 	}
 
+	/**
+	 * Get sys unique id
+	 * @return id
+	 */
 	public Integer getId(){
 		return id;
 	}
 
+	/**
+	 * Get type from sys value
+	 * @return type
+	 */
 	public Integer getType(){
 		return type;
 	}
 
+	/**
+	 * Obtain all the information as one string.
+	 * @return all values as a string
+	 */
 	@Override
  	public String toString(){
 		return 
