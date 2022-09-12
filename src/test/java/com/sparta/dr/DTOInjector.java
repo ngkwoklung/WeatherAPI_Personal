268c166cade269b95b3620b217fc5c6ec0f318e5
Roshni <roshnihpatel@outlook.com>
2022-09-12 16:09:44 +0100

    Started mocking for DTOInjectionTest

--- src/test/java/com/sparta/dr/DTOInjector.java
+++ src/test/java/com/sparta/dr/DTOInjector.java
@@ -3,9 +3,36 @@ package com.sparta.dr;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.dr.DTO.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DTOInjector {
+
+
+
+    public static void main(String[] args) {
+        System.out.println(connection().toString());
+        // (GET https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=a39a8ef364461dd7292792ea50bba6a1) 200
+    }
+
+    private static HttpResponse connection(){
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("https://api.openweathermap.org/data/2.5/weather?lat=0&lon=0&appid=a39a8ef364461dd7292792ea50bba6a1"))
+                .build();
+
+        HttpResponse<String> response =null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
+    }
 
     public static WeatherResponseDTO injectDTO(HttpResponse<String> response){
         WeatherResponseDTO weatherDTO = new WeatherResponseDTO();
--- src/test/java/com/sparta/dr/DTOInjectorTests.java
+++ src/test/java/com/sparta/dr/DTOInjectorTests.java
@@ -1,4 +1,24 @@
 package com.sparta.dr;
 
+import com.sparta.dr.DTO.WeatherResponseDTO;
+import com.sparta.dr.connectionmanager.ConnectionManager;
+import net.bytebuddy.agent.VirtualMachine;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import java.net.http.HttpResponse;
+import static org.hamcrest.MatcherAssert.*;
+
 public class DTOInjectorTests {
+
+    @Test
+    @DisplayName("Check ")
+    void check() {
+//        Mockito.when(ConnectionManager.getResponse("a")).thenReturn("{\"coord\":{\"lon\":0,\"lat\":0},\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"base\":\"stations\",\"main\":{\"temp\":294.82,\"feels_like\":295.55,\"temp_min\":294.82,\"temp_max\":294.82,\"pressure\":984,\"humidity\":96},\"visibility\":10000,\"wind\":{\"speed\":5.22,\"deg\":189,\"gust\":4.9},\"rain\":{\"1h\":0.25},\"clouds\":{\"all\":91},\"dt\":1662973919,\"sys\":{\"type\":2,\"id\":2008684,\"sunrise\":1662961990,\"sunset\":1663005590},\"timezone\":0,\"id\":6295630,\"name\":\"Globe\",\"cod\":200}");
+//        WeatherResponseDTO weatherDTO = DTOInjector.injectDTO(ConnectionManager.getResponse("a"));
+//        Assertions.assertNotNull(weatherDTO);
+
+    }
 }
