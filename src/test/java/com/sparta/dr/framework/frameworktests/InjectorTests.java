ba900638327954ff4da12be6e2c81202e49eafb7
Roshni <roshnihpatel@outlook.com>
2022-09-13 14:57:24 +0100

    deleted injector methods for all dto except for the WeatherResponseDTO

--- src/test/java/com/sparta/dr/framework/frameworktests/InjectorTests.java
+++ src/test/java/com/sparta/dr/framework/frameworktests/InjectorTests.java
@@ -12,11 +12,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
 public class InjectorTests {
     private static String APIKEY = PropertiesLoader.getProperty("apikey");
+
     private static HttpResponse connection(){
         var client = HttpClient.newHttpClient();
         var request = HttpRequest
@@ -32,64 +34,14 @@ public class InjectorTests {
         return response;
     }
 
-
     @Nested
     @DisplayName("Checking all injector methods are not null")
-    class Injector{
+    class Injector {
         @Test
         @DisplayName("Given a HttpRequest check that Weather injector return a dto that is not null")
         void givenAHttpRequestCheckThatWeatherInjectorReturnADtoThatIsNotNull() {
             WeatherResponse weatherDTO = com.sparta.dr.framework.injector.Injector.injectWeatherDTO(connection());
             Assertions.assertNotNull(weatherDTO);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that cloud injector return a dto that is not null")
-        void givenAHttpRequestCheckThatCloudInjectorReturnADtoThatIsNotNull() {
-            Clouds clouds = com.sparta.dr.framework.injector.Injector.injectCloudDTO(connection());
-            Assertions.assertNotNull(clouds);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that Main injector return a dto that is not null")
-        void givenAHttpRequestCheckThatMainInjectorReturnADtoThatIsNotNull() {
-            Main main = com.sparta.dr.framework.injector.Injector.injectMainDTO(connection());
-            Assertions.assertNotNull(main);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that Coord injector return a dto that is not null")
-        void givenAHttpRequestCheckThatCoordInjectorReturnADtoThatIsNotNull() {
-            Coord coord = com.sparta.dr.framework.injector.Injector.injectCoordDTO(connection());
-            Assertions.assertNotNull(coord);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that Rain injector return a dto that is not null")
-        void givenAHttpRequestCheckThatRainInjectorReturnADtoThatIsNotNull() {
-            Rain rain = com.sparta.dr.framework.injector.Injector.injectRainDTO(connection());
-            Assertions.assertNotNull(rain);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that Sys injector return a dto that is not null")
-        void givenAHttpRequestCheckThatSysInjectorReturnADtoThatIsNotNull() {
-            Sys sys = com.sparta.dr.framework.injector.Injector.injectSysDTO(connection());
-            Assertions.assertNotNull(sys);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that WeatherItem injector return a dto that is not null")
-        void givenAHttpRequestCheckThatWeatherItemInjectorReturnADtoThatIsNotNull() {
-            WeatherItem weatherItem = com.sparta.dr.framework.injector.Injector.injectWeatherItemDTO(connection());
-            Assertions.assertNotNull(weatherItem);
-        }
-
-        @Test
-        @DisplayName("Given a HttpRequest check that WeatherResponseDTO injector return a dto that is not null")
-        void givenAHttpRequestCheckThatWeatherResponseDTOInjectorReturnADtoThatIsNotNull() {
-            WeatherResponse weatherResponseDTO = com.sparta.dr.framework.injector.Injector.injectWeatherDTO(connection());
-            Assertions.assertNotNull(weatherResponseDTO);
         }
     }
 }
