05575b49e0f0f87e3863eb647bec845f9413cf76
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-09-13 10:12:27 +0100

    Added java doc to injectDTO method

--- src/test/java/com/sparta/dr/DTOInjector.java
+++ src/test/java/com/sparta/dr/DTOInjector.java
@@ -9,6 +9,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class DTOInjector {
+    /**
+     * Fills DTO array with all the relevant information that has been downloaded from the website.
+     *
+     * Pass HttpResponse in order to make the injector work successfully.
+     * @param response
+     * @return DTO object
+     */
     public static WeatherResponseDTO injectDTO(HttpResponse<String> response){
         WeatherResponseDTO weatherDTO = new WeatherResponseDTO();
         ObjectMapper objectMapper = new ObjectMapper();
