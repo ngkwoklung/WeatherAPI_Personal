44c4786a8de239395ced88cf749f44ae6932d0cb
Mohamed Lagha <mohamedlagha27@gmail.com>
2022-09-14 11:41:16 +0100

    added user stories for all existing helper methods

--- src/test/java/com/sparta/dr/cucumber/MyStepdefs.java
+++ src/test/java/com/sparta/dr/cucumber/MyStepdefs.java
@@ -1,5 +1,6 @@
 package com.sparta.dr.cucumber;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -51,5 +52,105 @@ public class MyStepdefs {
 
     @Then("the correct Zip Id is passed to the URL")
     public void theCorrectZipIdIsPassedToTheURL() {
+    }
+
+    @Then("the Weather DTO should have valid populated values")
+    public void theWeatherDTOShouldHaveValidPopulatedValues() {
+    }
+
+    @Then("the DTO should have the correct Weather Id")
+    public void theDTOShouldHaveTheCorrectWeatherId() {
+    }
+
+    @Given("that I call the API with a Date")
+    public void thatICallTheAPIWithADate() {
+    }
+
+    @Then("the DTO should have the today's date")
+    public void theDTOShouldHaveTheTodaySDate() {
+    }
+
+    @Given("that I call the API with a Sunrise Time and a Sunset Time")
+    public void thatICallTheAPIWithASunriseTimeAndASunsetTime() {
+    }
+
+    @Then("the DTO should have a Sunrise Time of today's date")
+    public void theDTOShouldHaveASunriseTimeOfTodaySDate() {
+    }
+
+    @And("the DTO should have a Sunset Time of today's date")
+    public void theDTOShouldHaveASunsetTimeOfTodaySDate() {
+    }
+
+    @Given("that I call the API with a Time Zone")
+    public void thatICallTheAPIWithATimeZone() {
+    }
+
+    @Then("the DTO should have the a time zone within the correct range")
+    public void theDTOShouldHaveTheATimeZoneWithinTheCorrectRange() {
+    }
+
+    @Given("that I call the API with a City Name and a Country Name")
+    public void thatICallTheAPIWithACityNameAndACountryName() {
+    }
+
+    @Then("the DTO should have the correct City Id")
+    public void theDTOShouldHaveTheCorrectCityId() {
+    }
+
+    @And("the DTO should have the correct Country Id")
+    public void theDTOShouldHaveTheCorrectCountryId() {
+    }
+
+    @Given("that I call the API with a Visibility")
+    public void thatICallTheAPIWithAVisibility() {
+    }
+
+    @Then("the DTO should have a Visibility within the correct range")
+    public void theDTOShouldHaveAVisibilityWithinTheCorrectRange() {
+    }
+
+    @Given("that I call the API with a Wind")
+    public void thatICallTheAPIWithAWind() {
+    }
+
+    @Then("the DTO should have a Wind Degree within the correct range")
+    public void theDTOShouldHaveAWindDegreeWithinTheCorrectRange() {
+    }
+
+    @And("the DTO should have a Wind Gust within the correct range")
+    public void theDTOShouldHaveAWindGustWithinTheCorrectRange() {
+    }
+
+    @And("the DTO should have a Wind Speed")
+    public void theDTOShouldHaveAWindSpeed() {
+    }
+
+    @Given("that I call the API with a Snow Volume for the last {int} hour, mm")
+    public void thatICallTheAPIWithASnowVolumeForTheLastHourMm(int arg0) {
+    }
+
+    @Then("the DTO should have a Snow Volume within the correct range")
+    public void theDTOShouldHaveASnowVolumeWithinTheCorrectRange() {
+    }
+
+    @Given("that I call the API with a Snow Volume for the last {int} hours, mm")
+    public void thatICallTheAPIWithASnowVolumeForTheLastHoursMm(int arg0) {
+    }
+
+    @Given("that I call the API with a Rain Volume")
+    public void thatICallTheAPIWithARainVolume() {
+    }
+
+    @Then("the DTO should have a Rain Volume within the correct range")
+    public void theDTOShouldHaveARainVolumeWithinTheCorrectRange() {
+    }
+
+    @Given("that I call the API with a Cloud value")
+    public void thatICallTheAPIWithACloudValue() {
+    }
+
+    @Then("the DTO should have a Cloud value within the correct range")
+    public void theDTOShouldHaveACloudValueWithinTheCorrectRange() {
     }
 }
